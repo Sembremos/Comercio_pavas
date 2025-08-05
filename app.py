6b6819f7b7549efec94712cb0935e3ab477c312a
Sembremos <sembremoseguridad@gmail.com>
2025-08-05 14:48:35 -0600

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.title("Encuesta sobre Seguridad para Comercios en Pavas")
 st.markdown(
     """
     <div style="text-align: justify;">
-    **Objetivo:** Recopilar información cualitativa sobre las dinámicas de asaltos y robos en las zonas comerciales de Pavas. Los datos son anónimos, confidenciales y serán utilizados exclusivamente para proponer mejoras en las estrategias de seguridad de la Fuerza Pública.
+    El objetivo de esta encuesta es recopilar información cualitativa sobre las dinámicas de asaltos y robos en las zonas comerciales de Pavas. Los datos son anónimos, confidenciales y serán utilizados exclusivamente para proponer mejoras en las estrategias de seguridad de la Fuerza Pública.
     </div>
     """,
     unsafe_allow_html=True
