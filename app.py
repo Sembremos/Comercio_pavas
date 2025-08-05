988a73356d4a60f598b6ab9e9eb9f089f544f496
Sembremos <sembremoseguridad@gmail.com>
2025-08-05 14:47:24 -0600

    Update app.py

--- app.py
+++ app.py
@@ -14,12 +14,15 @@ st.image("logo_pavas.png", width=700)
 
 # --- Título y descripción de la encuesta ---
 st.title("Encuesta sobre Seguridad para Comercios en Pavas")
-st.markdown("""
-**El Objetivo de esta encuesta es recopilar información cualitativa sobre las dinámicas de asaltos y robos en las zonas comerciales de Pavas. Los datos son anónimos, confidenciales y serán utilizados exclusivamente para proponer mejoras en las estrategias de seguridad de la Fuerza Pública.
-</div>
-""",
-unsafe_allow_html=true
+st.markdown(
+    """
+    <div style="text-align: justify;">
+    **Objetivo:** Recopilar información cualitativa sobre las dinámicas de asaltos y robos en las zonas comerciales de Pavas. Los datos son anónimos, confidenciales y serán utilizados exclusivamente para proponer mejoras en las estrategias de seguridad de la Fuerza Pública.
+    </div>
+    """,
+    unsafe_allow_html=True
 )
+
 st.divider()
 
 # --- Diccionarios de opciones para las preguntas de selección múltiple ---
